a969e3339d698598fc1edc486143235bee3f854f
Nick Wynja <nick@nickwynja.com>
2018-12-29 01:45:32 +0000

    Sub and unsub

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.7
 
+RUN pip3 install mailmanclient
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,13 +1,48 @@
 from flask import Flask, redirect, request
+from mailmanclient import Client
+
 
 app = Flask(__name__)
 
 @app.route("/subscribe", methods = ['POST'])
-def subscribe(links=None):
-    print(request.form)
-    url = request.form['success_redirect_url']
-    return redirect(url, code=302)
+def subscribe():
+    email = request.form['email']
+    l = request.form['list_name']
+    name = request.form['name']
+    success_url = request.form['success_redirect_url']
+    error_url = request.form['error_redirect_url']
 
+    client = Client('http://172.22.199.2:8001/3.1', 'restadmin', 'restpass')
+    list = client.get_list(l)
+
+    try:
+        user = list.get_member(email)
+        return redirect(success_url, code=302)
+    except:
+        # not a user
+        try:
+            # http://docs.mailman3.org/projects/mailmanclient/en/3.1.0/apiref.html#mailmanclient._client.MailingList.subscribe
+            list.subscribe(email, name, True, True, True)
+            return redirect(success_url, code=302)
+        except:
+            print('error subscribing')
+            return redirect(error_url, code=302)
+
+@app.route("/unsubscribe", methods = ['POST'])
+def unsubscribe():
+    email = request.form['email']
+    l = request.form['list_name']
+    success_url = request.form['success_redirect_url']
+    error_url = request.form['error_redirect_url']
+
+    client = Client('http://172.22.199.2:8001/3.1', 'restadmin', 'restpass')
+    list = client.get_list(l)
+
+    try:
+        list.unsubscribe(email)
+        return redirect(success_url, code=302)
+    except:
+        return redirect(error_url, code=302)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
